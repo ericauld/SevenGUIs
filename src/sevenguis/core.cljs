62c867d1b4f0b2c8454811a96d97c15359fc327b
Eric Auld <ericauld@gmail.com>
2021-01-16 17:53:51 -0800

    Add html skeleton for crud gui

--- src/sevenguis/core.cljs
+++ src/sevenguis/core.cljs
@@ -5,7 +5,11 @@
     [clojure.string :as str]))
 
 
-
+(defn dates-out-of-order? [date-str1 date-str2]
+  (let [[month1 day1 year1] (str/split date-str1 "/")
+        [month2 day2 year2] (str/split date-str2 "/")]
+    (prn (str year1 month1 day1) (str year2 month2 day2))
+    (> (compare (str year1 month1 day1) (str year2 month2 day2)) 0)))
 
 (defn not-a-calendar-date? [date-str]
   (let [[month-str day-str year-str] (str/split date-str "/")
@@ -16,13 +20,13 @@
         parsed-year (.getFullYear date)]
     (not (and (= month parsed-month) (= day parsed-day) (= year parsed-year)))))
 
-(defn date-doesnt-match-regex? [date-str]
+(defn not-in-date-format? [date-str]
   (not (re-matches #"^\d{2}/\d{2}/\d{4}$" date-str)))
 
 (defn invalid-date-input? [date-str]
   (if (str/blank? date-str)
     false
-    (or (date-doesnt-match-regex? date-str) (not-a-calendar-date? date-str))))
+    (or (not-in-date-format? date-str) (not-a-calendar-date? date-str))))
 
 (defn celsius->fahrenheit [celsius-str]
   (if (str/blank? celsius-str)
@@ -38,15 +42,88 @@
           converted-val (-> fahrenheit-flt (- 32) (* 5) (/ 9))]
       (.toFixed converted-val 1))))
 
-(defn dates-out-of-order? [date-str1 date-str2]
-  (let [[month1 day1 year1] (str/split date-str1 "/")
-        [month2 day2 year2] (str/split date-str2 "/")]
-    (prn (str year1 month1 day1) (str year2 month2 day2))
-    (> (compare (str year1 month1 day1) (str year2 month2 day2)) 0)))
+(defn get-event-value [event]
+  (.. event -target -value))
+
+(defn crud []
+  (r/with-let [name-list (r/atom ["Smith John" "Jones Jane"])
+               first-name-input (r/atom nil)
+               surname-input (r/atom nil)]
+    [:div.gui
+     [:div.gui-title "CRUD"]
+     [:div.gui-main
+      [:div#prefix
+       [:div "Filter prefix:"]
+       [:input]]
+      [:div#name
+       [:div#first-name
+        [:div "Name:"]
+        [:input {:value     @first-name-input
+                 :on-change (fn [event]
+                              (let [user-input (get-event-value event)]
+                                (reset! first-name-input user-input)))}]]
+       [:div#surname
+        [:div "Surname:"]
+        [:input {:value     @surname-input
+                 :on-change (fn [event]
+                              (let [user-input (get-event-value event)]
+                                (reset! surname-input user-input)))}]]
+       [:button {:on-click
+                 #(let [first-name @first-name-input
+                        surname @surname-input]
+                    (if-not (or (str/blank? first-name)
+                                (str/blank? surname))
+                      (do (r/rswap! name-list conj (str surname " " first-name))
+                          (reset! first-name-input nil)
+                          (reset! surname-input nil))))}
+        "Create"]]
+      [:div#name-list
+       [:select {:size 5 :on-change nil}
+        (for [name @name-list :let [[last first] (str/split name " ")]]
+          [:option {:value name} (str last ", " first)])]
+       [:button "Update"]
+       [:button "Delete"]]]]))
+
 
 (defn timer []
-  )
-
+  (r/with-let [decimal-precision 1
+               seconds-between-ticks 0.1
+               milliseconds-between-ticks (* 1000 seconds-between-ticks)
+               clock-is-ticking (r/atom false)
+               seconds-until-stop (r/atom 60.0)
+               seconds-elapsed (r/atom 0.0)
+               time-is-up (fn [] (>= (+ @seconds-elapsed 0.1) @seconds-until-stop))
+               tick! (fn [] (r/rswap! seconds-elapsed (fn [x] (+ x seconds-between-ticks))))
+               ticking-process (r/atom nil)
+               stop-clock! (fn [] (reset! clock-is-ticking false)
+                             (js/clearInterval @ticking-process))
+               tick-if-time-remains! #((if (time-is-up) (stop-clock!) (tick!)))
+               start-clock! (fn [] (reset! clock-is-ticking true)
+                              (reset! ticking-process (js/setInterval tick-if-time-remains!
+                                                                      milliseconds-between-ticks)))]
+    [:div.gui
+     [:div.gui-title "Timer"]
+     [:div.gui-main
+      [:meter {:min 0 :max @seconds-until-stop :value @seconds-elapsed}]
+      [:div.gui-display (.toFixed @seconds-elapsed decimal-precision) "s"]
+      [:div#duration
+       [:div "Duration"]
+       [:input {:type      "range" :min 5 :max 600 :value @seconds-until-stop
+                :on-change (fn [event]
+                             (let [user-input (get-event-value event)]
+                               (reset! seconds-until-stop user-input)
+                               (if (and (not @clock-is-ticking)
+                                        (not (= @seconds-elapsed 0))
+                                        (< @seconds-elapsed @seconds-until-stop))
+                                 (start-clock!))))}]
+       [:div.gui-display (.toFixed (js/parseFloat @seconds-until-stop) 1) "s"]]
+      [:div#buttons
+       [:button#reset {:on-click #((reset! seconds-elapsed 0)
+                                   (js/clearInterval @ticking-process))}
+        "Reset"]
+       [:button#start {:on-click start-clock!}
+        "Start"]]]]
+    (finally (js/clearInterval @ticking-process))))
 
 (defn flight-booker []
   (r/with-let [flight-type (r/atom "oneway")
@@ -55,49 +132,50 @@
                return-date (r/atom nil)
                return-date-is-invalid (r/atom true)
                flight-is-booked (r/atom false)]
-    [:div {:class "gui"}
-     [:div {:class "gui-title"} "Flight Booker"]
-     [:div {:class "gui-main"}
+    [:div#flight-booker.gui
+     [:div.gui-title "Flight Booker"]
+     [:div.gui-main
       [:select {:value     @flight-type
                 :style     {:display "block"}
                 :disabled  @flight-is-booked
                 :on-change (fn [event]
-                             (let [selection (.. event -target -value)]
+                             (let [selection (get-event-value event)]
                                (reset! flight-type selection)))}
        [:option {:value "roundtrip"} "Round-trip"]
        [:option {:value "oneway"} "One-way"]]
-      [:div [:div "Departure"]
-       [:input {:class       "gui-display"
-                :value       @departure-date
-                :disabled    @flight-is-booked
-                :style       {:width            "6em"
-                              :background-color (if (and @departure-date-is-invalid
-                                                         (not (str/blank? @departure-date))) "#f1bebe")}
-                :placeholder "mm/dd/yyyy"
-                :on-change   (fn [event] (let [date-entered (.. event -target -value)]
-                                           (reset! departure-date-is-invalid (invalid-date-input? date-entered))
-                                           (reset! departure-date date-entered)))}]]
-      [:div {:hidden (if (= @flight-type "oneway") true)}
+      [:div#departure
+       [:div "Departure"]
+       [:input#departure-input.gui-display
+        {:value       @departure-date
+         :disabled    @flight-is-booked
+         :style       {:width            "6em"
+                       :background-color (if (and @departure-date-is-invalid
+                                                  (not (str/blank? @departure-date))) "#f1bebe")}
+         :placeholder "mm/dd/yyyy"
+         :on-change   (fn [event] (let [date-entered (get-event-value event)]
+                                    (reset! departure-date-is-invalid (invalid-date-input? date-entered))
+                                    (reset! departure-date date-entered)))}]]
+      [:div#return {:hidden (if (= @flight-type "oneway") true)}
        [:div "Return"]
-       [:input {:class       "gui-display"
-                :value       @return-date
-                :disabled    @flight-is-booked
-                :style       {:width            "6em"
-                              :background-color (if (and @return-date-is-invalid
-                                                         (not (str/blank? @return-date))) "#f1bebe")}
-                :placeholder "mm/dd/yyyy"
-                :on-change   (fn [event] (let [date-entered (.. event -target -value)]
-                                           (reset! return-date-is-invalid (invalid-date-input? date-entered))
-                                           (reset! return-date date-entered)))}]]
+       [:input#return-input.gui-display
+        {:value       @return-date
+         :disabled    @flight-is-booked
+         :style       {:width            "6em"
+                       :background-color (if (and @return-date-is-invalid
+                                                  (not (str/blank? @return-date))) "#f1bebe")}
+         :placeholder "mm/dd/yyyy"
+         :on-change   (fn [event] (let [date-entered (get-event-value event)]
+                                    (reset! return-date-is-invalid (invalid-date-input? date-entered))
+                                    (reset! return-date date-entered)))}]]
       [:button {:on-click #(reset! flight-is-booked true)
                 :disabled (or @flight-is-booked
                               @departure-date-is-invalid
                               (and (= @flight-type "roundtrip") @return-date-is-invalid)
                               (dates-out-of-order? @departure-date @return-date))}
        "Book!"]]
-     [:div {:hidden (not @flight-is-booked)}
+     [:div#success-message {:hidden (not @flight-is-booked)}
       [:div "Success! Your trip departs on " @departure-date
-       (if (= @flight-type "roundtrip") (str " and returns " @return-date))"."]
+       (if (= @flight-type "roundtrip") (str " and returns " @return-date)) "."]
       [:button {:on-click (fn [event] (do (reset! departure-date "") (reset! flight-is-booked false) (reset! return-date "")))}
        "Book again!"]]]))
 
@@ -105,33 +183,29 @@
   (r/with-let
     [celsius-input (r/atom "")
      fahrenheit-input (r/atom "")
-     celsius-update
-     (fn [event]
-       (let [input-str (.. event -target -value)
-             converted-str (-> input-str celsius->fahrenheit)]
-         (reset! celsius-input input-str)
-         (reset! fahrenheit-input converted-str)))
-     fahrenheit-update
-     (fn [event]
-       (let [input-str (.. event -target -value)
-             converted-str (-> input-str fahrenheit->celsius)]
-         (reset! fahrenheit-input input-str)
-         (reset! celsius-input converted-str)))]
-    [:div {:class "gui"}
-     [:div {:class "gui-title"} "Temp Converter"]
-     [:div {:class "gui-main"}
-      [:div
-       [:input {:class     "gui-display"
-                :style     {:width "3.5em"}
-                :on-change celsius-update
-                :value     @celsius-input}]
+     celsius-update (fn [event]
+                      (let [input-str (get-event-value event)
+                            converted-str (-> input-str celsius->fahrenheit)]
+                        (reset! celsius-input input-str)
+                        (reset! fahrenheit-input converted-str)))
+     fahrenheit-update (fn [event]
+                         (let [input-str (get-event-value event)
+                               converted-str (-> input-str fahrenheit->celsius)]
+                           (reset! fahrenheit-input input-str)
+                           (reset! celsius-input converted-str)))]
+    [:div#temp-converter.gui
+     [:div.gui-title "Temp Converter"]
+     [:div.gui-main
+      [:div#celsius
+       [:input.gui-display {:style     {:width "3.5em"}
+                            :on-change celsius-update
+                            :value     @celsius-input}]
        [:div "Celsius"]]
-      [:div [:b "   "]]
-      [:div
-       [:input {:class     "gui-display"
-                :style     {:width "3.5em"}
-                :on-change fahrenheit-update
-                :value     @fahrenheit-input}]
+      [:div#space-between [:b "   "]]
+      [:div#fahrenheit
+       [:input.gui-display {:style     {:width "3.5em"}
+                            :on-change fahrenheit-update
+                            :value     @fahrenheit-input}]
        [:div "Fahrenheit"]]]]))
 
 (defn counter []
@@ -148,17 +222,16 @@
 
 (defn home-page []
   [:div {:style {:text-align "center"}}
-   [:div {:class "title" :style {:justify-content "space-around"}}
+   [:div.title {:style {:justify-content "space-around"}}
     [:h2 "Seven GUIs in Reagent"]]
-   [:div {:class "wrapper"}
+   [:div.wrapper
     [counter]
     [temp-converter]
     [flight-booker]
-    [:div {:class "gui"} "Four"]
-    [:div {:class "gui"} "Five"]
-    [:div {:class "gui"} "Six"]
-    [:div {:class "gui"} "Seven"]]])
-
+    [timer]
+    [crud]
+    [:div.gui "Six"]
+    [:div.gui "Seven"]]])
 
 (defn mount-root []
   (d/render [home-page] (.getElementById js/document "app")))
